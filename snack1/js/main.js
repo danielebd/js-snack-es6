e1ae56813013936bface2ba2d24981d8cb68fd98
danielebd <danielebalebalestro@gmail.com>
2023-03-08 16:38:58 +0100

    first commit

---
+++ snack1/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const invited = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+
+//console.log(element, index);
+const detailsIvited = invited.map((element, index) => {
+    //console.log(element, index);
+    return {tavolo: 'Tavolo Vip', nome: element, posto: index + 1} ;
+})
+
+console.log(detailsIvited)
---
+++ snack1/readme.md
@@ -0,0 +1,2 @@
+creare un'array con all'interno il nome degli invitati
+con method map creiamo un nuovo arrey che contenga degli oggetti in cui ognuno abbia: (nome tavolo, nome ospite, numero posto) 
