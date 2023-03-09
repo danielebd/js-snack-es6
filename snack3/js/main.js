1e50a49515ae458fbb84c32345c33bb5cbdc299f
danielebd <danielebalebalestro@gmail.com>
2023-03-09 16:17:36 +0100

    snack3 creazione array

--- snack3/js/main.js
+++ snack3/js/main.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const bikes = [
+    {
+        name: 'nome1',
+        weightkg: 4
+    },
+    {
+        name: 'nome2',
+        weightkg: 3
+    },
+    {
+        name: 'nome3',
+        weightkg: 4.5
+    },
+    {
+        name: 'nome4',
+        weightkg: 2
+    },
+    {
+        name: 'nome5',
+        weightkg: 7
+    },
+]
