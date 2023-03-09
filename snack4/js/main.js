45754c454227ce6e93236220643e9a19bdb1f955
danielebd <danielebalebalestro@gmail.com>
2023-03-09 17:51:27 +0100

    array teams

--- snack4/js/main.js
+++ snack4/js/main.js
@@ -1,1 +1,24 @@
 'use strict'
+
+const teams = [
+    {
+        name: 'inter',
+        score: 0,
+        foulPlay: 0
+    },
+    {
+        name: 'milan',
+        score: 0,
+        foulPlay: 0
+    },
+    {
+        name: 'juve',
+        score: 0,
+        foulPlay: 0
+    },
+    {
+        name: 'napoli',
+        score: 0,
+        foulPlay: 0
+    },
+]
